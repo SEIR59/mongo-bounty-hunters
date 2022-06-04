4d83a8993a882d3a6fbae9ec7ec03414d9c1c761
idbic <dbicga@gmail.com>
2022-06-04 08:51:11 -0500

    answers updated

---
+++ bounties.js
@@ -0,0 +1,11 @@
+db.bounties.insert(
+    {
+      name: 'Han Solo',
+      wantedFor : 'Owing money',
+      client : 'Jabba the Hut',
+      reward : 1000000,
+      ship: 'Millennium Falcon',
+      hunters :['Bobba Fett', 'Dengar', 'IG-88', 'Zuckuss', 'Greedo', 'Bossk', '4-LOM'],
+      captured: false
+    }
+    )
